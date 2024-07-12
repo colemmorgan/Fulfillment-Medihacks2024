1346424ad6b7a4f17a8c9002dc67c749a457da9a
Savio Xavier <savioxavier112@gmail.com>
2024-07-12 18:57:02 +0530

    feat: add join via game id feature

--- src/pages/Trivia.tsx
+++ src/pages/Trivia.tsx
@@ -6,6 +6,8 @@ import { collection, addDoc } from "firebase/firestore";
 const Trivia: React.FC = () => {
   const [gameId, setGameId] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [externalGameId, setExternalGameId] = useState("");
+
   const navigate = useNavigate();
 
   const startNewGame = async () => {
@@ -31,17 +33,47 @@ const Trivia: React.FC = () => {
     }
   };
 
+  const handleJoinExternalGame = (event) => {
+    event.preventDefault();
+    navigate(`/newgame?id=${externalGameId}`);
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">Trivia Game</h1>
       {!gameId ? (
-        <button
-          onClick={startNewGame}
-          disabled={isLoading}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        >
-          {isLoading ? "Creating Game..." : "Start New Game"}
-        </button>
+        <div className="flex flex-col">
+          <button
+            onClick={startNewGame}
+            disabled={isLoading}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            {isLoading ? "Creating Game..." : "Start New Game"}
+          </button>
+
+          <div className="h-0.5 my-4 flex self-stretch bg-blue-300"></div>
+
+          <div>
+            <form onSubmit={handleJoinExternalGame}>
+              <div className="flex gap-x-2">
+                <input
+                  className="bg-gray-50 hover:bg-gray-100 text-blue-600 border-2 border-blue-500 font-bold py-2 px-4 rounded"
+                  type="text"
+                  placeholder="Or enter a game ID"
+                  value={externalGameId}
+                  onChange={(event) => setExternalGameId(event.target.value)}
+                />
+                <button
+                  className="bg-blue-500 py-2 px-4 rounded text-white hover:bg-blue-600 transition-colors duration-200 border-2 border-transparent"
+                  type="submit"
+                  onClick={handleJoinExternalGame}
+                >
+                  Join
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       ) : (
         <div className="text-center">
           <p className="mb-4">Game ID: {gameId}</p>
